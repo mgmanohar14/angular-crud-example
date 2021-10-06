401708c7feed4e4257eb1e8122eaf6abe2d42855
mgmanohar14 <61844431+mgmanohar14@users.noreply.github.com>
2021-10-06 04:55:20 -0700

    added src folder

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './crud/home/home.component';
+import { DetailsComponent } from './crud/details/details.component';
+import { CreateComponent } from './crud/create/create.component';
+import { UpdateComponent } from './crud/update/update.component';
+
+
+const routes: Routes = [
+  { path: 'crud', redirectTo: 'crud/home', pathMatch: 'full'},
+  { path: 'crud/home', component: HomeComponent },
+  { path: 'crud/details/:productId', component: DetailsComponent },
+  { path: 'crud/create', component: CreateComponent },
+  { path: 'crud/update/:productId', component: UpdateComponent } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
